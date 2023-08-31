1b2a3968db5ed769e8e1c7d7c7092fc52b589dd7
Gokul Singh <62161468+Goki27@users.noreply.github.com>
2023-09-01 01:25:54 +0530

    Finishefed the game

--- Assets/scripts/Bird.cs
+++ Assets/scripts/Bird.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Bird : MonoBehaviour
 
@@ -17,10 +18,12 @@ public class Bird : MonoBehaviour
     private void Start()
     {
     
+
       lineRenderer = GetComponent<LineRenderer>();
       lineRenderer.SetPosition(0,transform.position);
         StartPosition = transform.position;
         lineRenderer.SetPosition(1,transform.position);
+       lineRenderer.enabled = false;
     }
 
 
@@ -30,6 +33,7 @@ public class Bird : MonoBehaviour
         Vector3 directionMagnitude=StartPosition-transform.position;
         GetComponent<Rigidbody2D>().AddForce(directionMagnitude*launchPower);
         GetComponent<Rigidbody2D>().gravityScale = 1;
+        lineRenderer.enabled = false;   
     }
 
     void OnMouseDrag()
@@ -40,10 +44,26 @@ public class Bird : MonoBehaviour
         if(Vector2.Distance(destination, StartPosition) > maxDragDistance)
         transform.position = destination;
         destination=Vector3.MoveTowards(StartPosition, destination,maxDragDistance);
+        lineRenderer.enabled = true;
     }
     // Update is called once per frame
     void Update()
     {
+
+        if (FindAnyObjectByType<Enemy>(FindObjectsInactive.Exclude) == null)
+        {
+            var levelToLoad = SceneManager.GetActiveScene().buildIndex + 1;
+            SceneManager.LoadScene(levelToLoad);
+        }
         
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        Invoke(nameof(RloadLevel),5);
+    }
+    private void RloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
